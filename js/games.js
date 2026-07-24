b0a951547659d6a2aeab8d6d3b93a7f3a0c3a5f4
bierzoparaiso-lgtm <bierzoparaiso@gmail.com>
2026-07-24 16:48:50 +0200

    Create games.js

---
+++ js/games.js
@@ -0,0 +1,20 @@
+const GAMES = [
+    {
+        id: 'coinflip',
+        nameKey: 'game_heads_tails',
+        icon: '🪙',
+        endpoint: '/api/play/coinflip',
+        minBet: 100,
+        maxBet: 5000,
+        enabled: true
+    },
+    {
+        id: 'roulette',
+        nameKey: 'game_roulette',
+        icon: '🎡',
+        endpoint: '/api/play/roulette',
+        minBet: 100,
+        maxBet: 5000,
+        enabled: true
+    }
+];
